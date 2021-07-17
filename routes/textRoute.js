b32674060bb5c8ff74249eb1a99a0a150a644ce0
justfawzi <fawzichibah@gmail.com>
2021-07-17 15:54:10 +0100

    challenge completed

--- routes/textRoute.js
+++ routes/textRoute.js
@@ -6,9 +6,9 @@ module.exports = (app) => {
     app.post('/text', textController.addText);
     app.get('/text', textController.getText);
     app.put('/text/:textId', textController.editText);
-    app.get('/text/text/:textId/count', textController.countWords);
+    app.get('/text/:textId/count', textController.countWords);
     app.get('/text/:textId/count/:language', textController.countWordsLang);
-    app.post(' /text/search?q=', textController.fuzzySearch);
+    app.post('/text/search?q=', textController.fuzzySearch);
     app.get('/text/mostOccurrent', textController.mostOccurentWord);
 
 }
